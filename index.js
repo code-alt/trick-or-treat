7268d39aed34009e5b3af1333d5f769ecd5fa7cf
code-alt <74145728+code-alt@users.noreply.github.com>
2021-02-03 11:29:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname));
-app.listen(3000);
+app.get("/",(req,res)=>{res.sendFile("./index.html")});
+app.listen(8080);
