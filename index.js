73ecbb55fd4f4acf14fc9cb721b9fd824da00407
code-alt <74145728+code-alt@users.noreply.github.com>
2021-02-03 11:35:31 -0600

    oof.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=>{res.sendFile(__dirname + "/index.html")});
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(port);
